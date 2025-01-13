23c8097c9f1cccbc7f84d0a39b45b6c3de11eaef
Ethan Chen <echen@lcs.on.ca>
2025-01-13 09:41:57 -0500

    changed interface to fit sum, created firstNumber and secondNumber variables and sum, and binded them to the steppers

--- FindingSums/ContentView.swift
+++ FindingSums/ContentView.swift
@@ -8,12 +8,12 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
-    @State var base: Int = 1
-    // @State makes the interface change in real-time to the property's value. A property wrapper
+    @State var firstNumber: Int = 1
+    @State var secondNumber: Int = 1
     
     // MARK: Computed properties
-    var squared: Int{
-        return base*base
+    var sum : Int{
+        return firstNumber+secondNumber
     }
     
     var body: some View {
@@ -24,50 +24,37 @@ struct ContentView: View {
             
             HStack(alignment: .top) {
  
-                Text("(\(base))")
-                    .font(.system(size: 96))
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.5)
+                Spacer()
  
-                Text("2")
-                    .font(.system(size: 44))
- 
-                Text("=")
-                    .font(.system(size: 96))
- 
-                Text("\(squared)")
-                    .font(.system(size: 96))
+                Text("\(firstNumber)")
+                    .font(.system(size: 60))
                     .lineLimit(1)
                     .minimumScaleFactor(0.5)
             }
             
-            Stepper(value: $base, label: {
-                Text("Base")
+            Stepper(value: $firstNumber, label: {
+                Text("Select First Number")
             })
             //allows user to provide input on + or -
             //$ is a binder, connecting stepper to value of base
             
             HStack(alignment: .top) {
  
-                Text("(\(base))")
-                    .font(.system(size: 96))
+                Text("+")
+                    .font(.system(size: 60))
                     .lineLimit(1)
                     .minimumScaleFactor(0.5)
+ Spacer()
  
-                Text("2")
-                    .font(.system(size: 44))
  
-                Text("=")
-                    .font(.system(size: 96))
- 
-                Text("\(squared)")
-                    .font(.system(size: 96))
+                Text("\(secondNumber)")
+                    .font(.system(size: 60))
                     .lineLimit(1)
                     .minimumScaleFactor(0.5)
             }
             
-            Stepper(value: $base, label: {
-                Text("Base")
+            Stepper(value: $secondNumber, label: {
+                Text("Select Second Number")
             })
             Spacer()
         }
