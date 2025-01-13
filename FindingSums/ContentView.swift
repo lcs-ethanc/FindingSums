ff39137331a9c3805b0903ca754644ce7b0fde4c
Ethan Chen <echen@lcs.on.ca>
2025-01-13 09:31:37 -0500

    set up project and used stepperexample code as a reference

--- FindingSums/ContentView.swift
+++ FindingSums/ContentView.swift
@@ -1,24 +1,57 @@
 //
 //  ContentView.swift
-//  FindingSums
+//  StepperExample
 //
-//  Created by Pak Ching Ethan Chen on 2025-01-13.
-//
-
+ 
 import SwiftUI
-
+ 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var base: Int = 1
+    // @State makes the interface change in real-time to the property's value. A property wrapper
+    
+    // MARK: Computed properties
+    var squared: Int{
+        return base*base
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+ 
+                Text("(\(base))")
+                    .font(.system(size: 96))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+ 
+                Text("2")
+                    .font(.system(size: 44))
+ 
+                Text("=")
+                    .font(.system(size: 96))
+ 
+                Text("\(squared)")
+                    .font(.system(size: 96))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+            }
+            
+            Stepper(value: $base, label: {
+                Text("Base")
+            })
+            //allows user to provide input on + or -
+            //$ is a binder, connecting stepper to value of base
+            Spacer()
         }
         .padding()
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
+ 
