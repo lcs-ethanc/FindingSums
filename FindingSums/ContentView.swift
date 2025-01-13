cd3a877c767fef0898dc850867e1967127a68914
Ethan Chen <echen@lcs.on.ca>
2025-01-13 09:33:15 -0500

    made a new v stack

--- FindingSums/ContentView.swift
+++ FindingSums/ContentView.swift
@@ -17,37 +17,70 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            
-            Spacer()
-            
-            HStack(alignment: .top) {
- 
-                Text("(\(base))")
-                    .font(.system(size: 96))
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.5)
- 
-                Text("2")
-                    .font(.system(size: 44))
- 
-                Text("=")
-                    .font(.system(size: 96))
- 
-                Text("\(squared)")
-                    .font(.system(size: 96))
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.5)
+        VStack{
+            VStack {
+                
+                Spacer()
+                
+                HStack(alignment: .top) {
+                    
+                    Text("(\(base))")
+                        .font(.system(size: 96))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                    
+                    Text("2")
+                        .font(.system(size: 44))
+                    
+                    Text("=")
+                        .font(.system(size: 96))
+                    
+                    Text("\(squared)")
+                        .font(.system(size: 96))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                }
+                
+                Stepper(value: $base, label: {
+                    Text("Base")
+                })
+                //allows user to provide input on + or -
+                //$ is a binder, connecting stepper to value of base
             }
+            .padding()
             
-            Stepper(value: $base, label: {
-                Text("Base")
-            })
-            //allows user to provide input on + or -
-            //$ is a binder, connecting stepper to value of base
-            Spacer()
+            VStack {
+                
+                Spacer()
+                
+                HStack(alignment: .top) {
+                    
+                    Text("(\(base))")
+                        .font(.system(size: 96))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                    
+                    Text("2")
+                        .font(.system(size: 44))
+                    
+                    Text("=")
+                        .font(.system(size: 96))
+                    
+                    Text("\(squared)")
+                        .font(.system(size: 96))
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                }
+                
+                Stepper(value: $base, label: {
+                    Text("Base")
+                })
+                //allows user to provide input on + or -
+                //$ is a binder, connecting stepper to value of base
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
  
