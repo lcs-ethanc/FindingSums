8060fd3a23fc6252f03a2d5b5c7ef3748e891579
Ethan Chen <echen@lcs.on.ca>
2025-01-13 09:30:46 -0500

    Initial Commit

---
+++ FindingSums/FindingSumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingSumsApp.swift
+//  FindingSums
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct FindingSumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
